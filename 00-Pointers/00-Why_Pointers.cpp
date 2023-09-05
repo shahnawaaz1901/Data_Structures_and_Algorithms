188e344927f5a9e22925667e675153963cc019d5
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-09-05 19:31:35 +0530

    Start Data Structures With Pointers

---
+++ 00-Pointers/00-Why_Pointers.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i = 10;
+    /*
+        When Ever We Create a Variable in Any Language System Follows Three Steps For that :
+        1. Allocate Memory for that Variable(Allocate Size According to Data Type)
+        2. Add an Entry for that Variable Corresponding to allocated Memory Address in 
+            Symbol Table
+        3. Fill that Memory Address with the value of Variable.
+    */
+
+   /*
+    Symbol Table : A table where Variable and it's Corresponding Memory Address is Stored
+                is Called Symbol Table
+   */
+}
