d12846778db4e5bef2b9dfdcfe671abdd2871793
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-09-05 19:40:07 +0530

    fetch Address of Variables

--- 00-Pointers/00-Why_Pointers.cpp
+++ 00-Pointers/00-Why_Pointers.cpp
@@ -1,17 +1,24 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
+int main()
+{
     int i = 10;
     /*
         When Ever We Create a Variable in Any Language System Follows Three Steps For that :
         1. Allocate Memory for that Variable(Allocate Size According to Data Type)
-        2. Add an Entry for that Variable Corresponding to allocated Memory Address in 
+        2. Add an Entry for that Variable Corresponding to allocated Memory Address in
             Symbol Table
         3. Fill that Memory Address with the value of Variable.
     */
 
-   /*
+    /*
     Symbol Table : A table where Variable and it's Corresponding Memory Address is Stored
-                is Called Symbol Table
-   */
+                 is Called Symbol Table
+    */
+    /*
+    For fetching Memory of a Perticular Variable Address Use '&' Before
+    Variable Name to Fetch Address
+    */
+
+    cout << &i << endl; // Prints Address of i in HexaDecimal Form
 }
