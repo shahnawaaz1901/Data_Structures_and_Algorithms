e2230ba6a6557d7e8b7cf67227fce4e3cdf5298b
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-09-05 19:47:21 +0530

    Create First Pointer

--- 00-Pointers/01-Create_Pointer.cpp
+++ 00-Pointers/01-Create_Pointer.cpp
@@ -1,5 +1,17 @@
 #include<iostream>
 using namespace std;
 int main(){
-    
+    /*
+        Pointers : Pointers Helps us to Store Address of Any Kind of DataType Variable.
+        Syntax :
+        data_type * pointer_name = &"variable_name"
+        Variable Name Not Require any Kind of Parenthesis
+    */
+
+    int i = 10;
+    // Create Pointer
+    int *pointer1 = &i;
+    // Because Pointer Stores Variable Address So that Both Statements Print Same Address
+    cout << &i << endl;
+    cout << pointer1 << endl;
 }
