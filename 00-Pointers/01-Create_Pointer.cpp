c4ccc7713c4e7f95c3b745967f71bc1a48d16c25
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-09-05 19:53:57 +0530

    Size of Pointers

--- 00-Pointers/01-Create_Pointer.cpp
+++ 00-Pointers/01-Create_Pointer.cpp
@@ -14,4 +14,12 @@ int main(){
     // Because Pointer Stores Variable Address So that Both Statements Print Same Address
     cout << &i << endl;
     cout << pointer1 << endl;
+
+    /* 
+        Like Other Data_Types Pointers Also Takes Space in Memory
+        Generally Pointers Have 8Bytes Memory For All Kind of Variables
+        Because Address of Any Kind of Variable is HexaDecimal So Require
+        Storage is 8Bytes Generally Sometimes 4bytes in Some Machines Depends
+        On Which Architecture machine Have
+    */
 }
