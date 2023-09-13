02048dac1cb7a8199a2576e14eece715bb05177e
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-09-13 19:19:06 +0530

    UnderStand Double Pointers

--- 00-Pointers/15-Double_Pointers.cpp
+++ 00-Pointers/15-Double_Pointers.cpp
@@ -1,5 +1,20 @@
 #include<iostream>
 using namespace std;
+
+void increment1(int **p){
+    p = p + 1;                      // Changes Not Reflected in our main Function
+}
+
+
+void increment2(int **p){
+    *p = *p + 1;                    // Changes Reflected in Our Main Function Because Changes is done Through Dereffer so that's why changes is reflected
+}
+
+void increment2(int **p){
+    **p = **p + 1;                  // Changes Reflected in Our main Function Because Changes is Done Through Double dereffer
+}
+
+
 int main(){
     int i = 10;
     // We We Write this
@@ -27,4 +42,18 @@ int main(){
         that data would be integer pointer
     */
 
+    // Both Prints Same Statement Address of y Because z is a Double Pointer Stores Address of y
+    cout << z << endl;
+    cout << &y << endl;
+
+
+    cout << *z << endl;         // Prints Value which y contains here y contains address of x
+    cout << *y << endl;         // prints Value which x contains here x contains value of 100
+    /*
+        This double dereference :
+        prints Value which x Contains because first dereffer goes for value of y which is 
+        address of x and second time dereffer goes for that address of x and prints value 
+        of that address.
+    */
+    cout << **z << endl;        
 }
